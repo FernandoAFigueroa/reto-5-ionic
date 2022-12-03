44d57ba99fe5b17db8b61093b934286325e79d28
Fernando A Figueroa <fernando.figueroa@corcoran.com>
2022-12-03 15:18:41 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'reto-5-ionic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
