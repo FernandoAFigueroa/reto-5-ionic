c00307f221729fe1eef70f5d98b26a1be1abce75
Fernando A Figueroa <fernando.figueroa@corcoran.com>
2022-12-04 14:44:41 -0600

    ionic/capacitor/android added and working

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.example.app',
   appName: 'reto-5-ionic',
   webDir: 'www',
   bundledWebRuntime: false
